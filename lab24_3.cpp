827995ec986ad1cd16aec1f16e610e204f73df7a
LimPrattana <vivo.twy0270@gmail.com>
2025-03-04 23:25:01 +0700

    lab24_3 com

--- lab24_3.cpp
+++ lab24_3.cpp
@@ -3,6 +3,20 @@
 using namespace std;
 
 //Write function count() here.
+int count(int a[], int s){
+	int c =0;
+	for(int i=0;i<s;i++){
+		bool u = true;
+		for(int j=0;j<i;j++){
+			if(a[i]==a[j]){
+			u = false;
+			break;
+		}
+	}
+	if(u) c++;
+	}
+	return c;
+}
 
 int main(){	
 	int data[] = {1,2,4,5,4,8,2,1,2,4,6,1,4,4,4,2,1,0,12}; 
